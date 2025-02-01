11d61c0a16d63c7effcc571688a87a6f14789b6f
Surachart01 <titlemy36@gmail.com>
2025-02-01 22:40:57 +0700

    commit

--- pdf.php
+++ pdf.php
@@ -1,93 +1,128 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-use Mpdf\Mpdf;
-
 try {
-    // ตั้งค่า default font เป็น THSarabun
-    $mpdf = new Mpdf();
-    $mpdf->charset_in = 'UTF-8';
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    $mpdf = new \Mpdf\Mpdf(['tempDir' => __DIR__ . '/tmp']);
+    $mpdf->SetTitle('ใบแจ้งซ่อม/รายงานผลการซ่อม/เบิกอะไหล่');
 
     $html = '
-        <style>
-            body{ font-family: "Garuda"; font-size: 10pt; }
+<style>
+    body { font-family: "Garuda", sans-serif; }
+    table { width: 100%; border-collapse: collapse; border: 1px solid black;}
+    th, td { border: 0px solid black; padding: 8px; text-align: left; }
+    .header { text-align: center; font-size: 20px; font-weight: bold; margin-bottom: 10px; }
+    .sub-header { text-align: center; font-size: 14px; margin-bottom: 20px; }
+    .section-title { font-size: 16px; font-weight: bold; background-color: #f2f2f2; padding: 5px; }
+    .signature { text-align: center; margin-top: 30px; }
+    .checkbox { display: inline-block; width: 12px; height: 12px; border: 1px solid black; margin-right: 5px; }
+</style>
 
-            p{  
-                margin-top: 10px;
-            }
-            table {
-                width: 100%;
-                border-collapse: collapse;
-            }
-            .header-table td {
-                vertical-align: top;
-            }
-            .header-title {
-                text-align: left;
-            }
-            .card-header {
-                background-color: yellow;
-                border: 1px solid black;
-                padding: 10px;
-                text-align: start;
-            }
-        </style>
-        <table class="header-table">
-            <tr>
-                <td class="header-title">
-                    <h3>ใบแจ้งซ่อม/รายงานผลการซ่อม/เบิกอะไหล่</h3>
-                    <p>(ครุภัณฑ์คอมพิวเตอร์และอุปกรณ์คอมพิวเตอร์)</p>
-                </td>
-                <td class="card-header">
-                    <p>พัสดุลงรับเลขที่...........</p>
-                    <p>วันที่...................</p>
-                </td>
-            </tr>
-        </table>
-        <p style="margin:5px 0px 5px 0px; text-align:center;">โทรศัพท์ : (036) 7854444 ต่อ 4530</p>
-        <table border="1">
-            <tr>
-                <td style="width: 50%;">
-                    <u>ข้อมูลส่งซ่อม</u>
-                    <p style="padding-top:10px">วันที่แจ้งซ่อม ........................................</p>
-                    <p style="padding-top:10px">ครุภัณฑ์ ............................................</p>
-                    <p style="padding-top:10px">ยี่ห้อ/รุ่น ...........................................</p>
-                    <p style="padding-top:10px">วันที่ได้รับ ..........................................</p>
-                </td>
-                <td style="width: 50%;">
-                    <p>เลขที่ส่งซ่อม.........................................</p>
-                    <p>หน่วยงาน...........................................</p>
-                    <p>IP ...............................................</p>
-                    <p>ราคา ................. ผู้จำหน่าย ...................</p>
-                </td>
-            </tr>
-            <tr>
-                <td colspan="2">
-                    <p>อาการ/สาเหตุ............................................................................................</p>
-                </td>
-            </tr>
-            <tr>
-                <td></td>
-                <td>
-                    <p>ผู้แจ้งซ่อม .............................................</p>
-                    <p style="margin-left:10px;">วันที่ .................................................</p>
-                </td>
-            </tr>
-        </table>
-    ';
-
-    $mpdf->WriteHTML($html);
-    $mpdf->Output('styled-example.pdf', 'I');
-} catch (\Throwable $th) {
-    echo $th->getMessage();
-}
+<div class="header">ใบแจ้งซ่อม/รายงานผลการซ่อม/เบิกอะไหล่</div>
+<div class="sub-header"><b>โรงพยาบาลพระนารายณ์มหาราช</b> โทรศัพท์: (036) 785444 ต่อ 4530</div>
 
 <table>
     <tr>
-        <td>การดำเนินการ.................................................................................</td>
-        <input type="checkbox" ><p>เบิกวัสดุ/อะไหล่</p>
+        <td><b>ข้อมูลแจ้งซ่อม</b></td>
+        <td>เลขที่แจ้งซ่อม: ______________________</td>
+    </tr>
+    <tr>
+        <td>วันที่แจ้งซ่อม: ____________________________</td>
+        <td>หน่วยงาน: ____________________________</td>
+    </tr>
+    <tr>
+        <td>ครุภัณฑ์: ____________________________</td>
+        <td>หมายเลข 7440-_______/_______/_______</td>
+    </tr>
+    <tr>
+        <td>ยี่ห้อ/รุ่น: ___________________________</td>
+        <td>IP: _______________________________</td>
+    </tr>
+    <tr>
+        <td>วันที่ได้รับ: ____________________________</td>
+        <td>ราคา: __________ ผู้จำหน่าย: _____________</td>
     </tr>
 </table>
+
+<table>
+    <tr>
+        <td><b>การดำเนินการ _______________________________________________________________________________________________________</b></td>
+    </tr>
+    <tr>
+        <td>
+            <input class="checkbox">เบิกวัสดุ/อะไหล่ ______________
+            <input class="checkbox"> ส่งซ่อมภายนอก
+            <input class="checkbox"> ส่งคืน
+            <input class="checkbox"> อื่นๆ: _________________________
+        </td>
+    </tr>
+</table>
+
+<table>
+    <tr>
+        <td class="section-title">ความเห็นของผู้ตรวจสอบช่างคอมพิวเตอร์</td>
+    </tr>
+    <tr>
+        <td>
+            ลงชื่อ: ____________________________ <br>
+            (นางสาวฐิติภาน์ มากสินธี) <br>
+            ตำแหน่ง: นักวิชาการคอมพิวเตอร์ปฏิบัติการ <br>
+            วันที่: ____________________________
+        </td>
+    </tr>
+</table>
+
+<table>
+    <tr>
+        <td class="section-title">ความเห็นของหัวหน้างานเทคโนโลยีสารสนเทศ</td>
+    </tr>
+    <tr>
+        <td>
+            ลงชื่อ: ____________________________ <br>
+            (นางฐิติกา เชื้ออินทร์) <br>
+            ตำแหน่ง: นักวิชาการคอมพิวเตอร์ปฏิบัติการ <br>
+            วันที่: ____________________________
+        </td>
+    </tr>
+</table>
+
+<table>
+    <tr>
+        <td class="section-title">ความเห็นผู้บริหารงานเทคโนโลยีสารสนเทศระดับสูง (CIO)</td>
+    </tr>
+    <tr>
+        <td>
+            ลงชื่อ: ____________________________ <br>
+            (นายอัมพล วิบูลย์ศักดิ์สกุล) <br>
+            ตำแหน่ง: เจ้าราชการเชี่ยวชาญ <br>
+            วันที่: ____________________________
+        </td>
+    </tr>
+</table>
+
+<table>
+    <tr>
+        <td class="section-title">งานพัสดุ: จัดซื้ออุปกรณ์ให้ศูนย์คอมแล้ว</td>
+    </tr>
+    <tr>
+        <td>
+            วันที่: ____________________________ <br>
+            ราคา: ____________________________ <br>
+            กำหนด: ____________________________ <br>
+            ลงชื่อ: ____________________________ <br>
+            ศูนย์คอมพิวเตอร์: รับอุปกรณ์แล้ว <br>
+            ลงชื่อ: ____________________________ <br>
+            วันที่: ____________________________
+        </td>
+    </tr>
+</table>
+
+';
+
+    // ใส่ HTML เข้าไปใน PDF
+    $mpdf->WriteHTML($html);
+
+    // แสดง PDF บนเบราว์เซอร์
+    $mpdf->Output('repair_report.pdf', 'I');
+} catch (\Throwable $th) {
+    echo $th;
+}
